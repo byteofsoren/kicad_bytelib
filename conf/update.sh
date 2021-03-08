beef647aa28e5133b6408cf2f166a849a5bd1b5a
byteofsoren <r00tr4t@gmail.com>
2021-03-08 22:23:16 +0100

    added a new update.sh that removes local paths form the symbols/footprints libaries.
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- conf/update.sh
+++ conf/update.sh
@@ -2,15 +2,25 @@
 
 # Updates the files for kicad so that they use the BYTELIB env
 ENV="uri \${BYTELIB}"
-echo $ENV
 localpath="uri /home/r00tr4t/.kicad/bytelib_kicad"
+
+
+
+# Sanitises the output an converts the / to \/ so it can be used by rexex
 sanetised=$(echo $localpath | sed 's/\//\\\//g')
-#uri \/home\/r00tr4t\/.kicad\/bytelib_kicad
-echo $sanetised
-footprint="fp-lib-table"
-symbols="sym-lib-table"
-# cat $symbols | sed -e "s/uri $localpath/uri \$\($ENV\)/g"
-# cat sym-lib-table | sed 's//uri ${BYTELIB}/g'
-cat $footprint | sed "s/$sanetised/$ENV/g"
-cat $symbols | sed "s/$sanetised/$ENV/g"
+footprint="fp-lib-table" # handles the footprints in KiCAD
+symbols="sym-lib-table"  # handles the symbols in kiCAD
+# Create templife with the corrected symbols files
+cat $symbols | sed "s/$sanetised/$ENV/g" > tempfile
+if [[ $(cat tempfile | wc -l) > 0 ]]; then
+	# move the tmpifle to the symbolssymbols  libary.
+	mv tempfile $symbols
+fi
+# Create templife with the corrected footprints files
+cat $footprint | sed "s/$sanetised/$ENV/g" > tempfile
+if [[ $(cat tempfile | wc -l) > 0 ]]; then
+	# move the tmpifle to the footprints libary.
+	mv tempfile $footprint
+fi
+
 
