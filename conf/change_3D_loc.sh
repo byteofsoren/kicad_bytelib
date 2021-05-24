0a4398518d8f77b502c843efedbabb225fc80be0
byteofsoren <r00tr4t@gmail.com>
2021-05-24 23:59:48 +0200

    before manipulaion
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- conf/change_3D_loc.sh
+++ conf/change_3D_loc.sh
@@ -4,11 +4,14 @@ INPUTDIR="../"
 
 # Search strings
 SERCHFTS=( "step" "stl" "vrl" "stp")
-SEARCHSTR="temp:"
+SEARCHSTR="model :temp:"
+REPLACESTR="model :$(BYTELIB)/3D/"
 
 # Find all files containing
 # └──> find .. -type f -name '*.kicad_mod' | while read fname; do cat "$fname" | grep 'temp' ; done
-find $INPUTDIR -type f -name '*.kicad_mod' | while read fname; do
-    cat $fname | grep $SEARCHSTR
-done
 
+find $INPUTDIR -type f -name '*.kicad_mod' | while read fname; do
+	echo $fname
+	cat $fname | grep "$SEARCHSTR"
+	sed -i "s/$SEARCHSTR/$REPLACESTR/" $fname
+done
