0ef8ae266ca051d19b5b7e522bc375cddf36a11d
byteofsoren <r00tr4t@gmail.com>
2021-05-25 02:41:08 +0200

    3D graphics is mostly supported in BYTELIB now
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- conf/change_3D_loc.sh
+++ conf/change_3D_loc.sh
@@ -4,7 +4,7 @@ INPUTDIR="../"
 
 
 
-
 find $INPUTDIR -type f -name '*.kicad_mod' | while read fname; do
-	sed -Ei 's/model :temp:(.+)/model "\$\{BYTELIB\}\/3D\/\1"/g' $fname
+	# sed -Ei 's/model :temp:(.+)/model "\$\{BYTELIB\}\/3D\/\1"/g' $fname
+	grep "model" $fname
 done
