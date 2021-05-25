6b6530f14308bcd9f28e69f367371847a7810f40
byteofsoren <r00tr4t@gmail.com>
2021-05-25 02:37:15 +0200

    New sed approach
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- conf/change_3D_loc.sh
+++ conf/change_3D_loc.sh
@@ -2,16 +2,9 @@
 
 INPUTDIR="../"
 
-# Search strings
-SERCHFTS=( "step" "stl" "vrl" "stp")
-SEARCHSTR="model :temp:"
-REPLACESTR="model :$(BYTELIB)/3D/"
 
-# Find all files containing
-# └──> find .. -type f -name '*.kicad_mod' | while read fname; do cat "$fname" | grep 'temp' ; done
+
 
 find $INPUTDIR -type f -name '*.kicad_mod' | while read fname; do
-	echo $fname
-	cat $fname | grep "$SEARCHSTR"
-	sed -i "s/$SEARCHSTR/$REPLACESTR/" $fname
+	sed -Ei 's/model :temp:(.+)/model "\$\{BYTELIB\}\/3D\/\1"/g' $fname
 done
